7fe1e284e4d47d0545fbb8e961b0ceb27f07452a
Youngsuh <yschin0508@gmail.com>
2021-11-15 14:02:08 +0900

    edit restpane clickshow

--- addWorkout.js
+++ addWorkout.js
@@ -1,20 +1,28 @@
 function onDisplayWorkout() {
   var clickshow = document.getElementById("clickshow");
-  var addButtons = document.getElementById("addButtons");
-
+  var midpane = document.getElementById("midpane");
+  var rightpane = document.getElementById("rightpane");
   if (clickshow.style.display === "none") {
     clickshow.style.display = "block";
   }
-  if (addButtons.style.display === "block") {
-    addButtons.style.display = "none";
+  if (midpane.style.display === "block") {
+    midpane.style.display = "none";
+  }
+  if (rightpane.style.display === "block") {
+    rightpane.style.display = "none";
   }
 }
 function ReturnNormal() {
   var clickshow = document.getElementById("clickshow");
+  var midpane = document.getElementById("midpane");
+  var rightpane = document.getElementById("rightpane");
   if (clickshow.style.display === "block") {
     clickshow.style.display = "none";
   }
-  if (addButtons.style.display === "none") {
-    addButtons.style.display = "block";
+  if (midpane.style.display === "none") {
+    midpane.style.display = "block";
+  }
+  if (rightpane.style.display === "none") {
+    rightpane.style.display = "block";
   }
 }
