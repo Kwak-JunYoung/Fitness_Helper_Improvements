b196c77d802aed0548b1e01041d2798f41800d14
Youngsuh <yschin0508@gmail.com>
2021-11-15 14:55:50 +0900

    1st frontend merge

--- addWorkout.js
+++ addWorkout.js
@@ -14,10 +14,14 @@ function onDisplayWorkout() {
 }
 function ReturnNormal() {
   var clickshow = document.getElementById("clickshow");
+  var clickshow1 = document.getElementById("restpane1");
   var midpane = document.getElementById("midpane");
   var rightpane = document.getElementById("rightpane");
   if (clickshow.style.display === "block") {
     clickshow.style.display = "none";
+  }
+  if (clickshow1.style.display === "block") {
+    clickshow1.style.display = "none";
   }
   if (midpane.style.display === "none") {
     midpane.style.display = "block";
@@ -25,4 +29,22 @@ function ReturnNormal() {
   if (rightpane.style.display === "none") {
     rightpane.style.display = "block";
   }
+}
+
+function diet() {
+  var clickshow = document.getElementById("restpane1");
+  var midpane = document.getElementById("midpane");
+  var rightpane = document.getElementById("rightpane");
+  if (clickshow.style.display === "none") {
+    clickshow.style.display = "block";
+  }
+  if (midpane.style.display === "block") {
+    midpane.style.display = "none";
+  }
+  if (rightpane.style.display === "block") {
+    rightpane.style.display = "none";
+  }
+}
+function dietinsert() {
+  window.open("diet.html", "add_diet", "width=500, height=600");
 }
