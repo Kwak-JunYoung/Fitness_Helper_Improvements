0a8132ba737a59d09b06d103fa5f5875990a4311
Youngsuh <yschin0508@gmail.com>
2021-11-22 03:32:21 +0900

    add user number change

--- EnterExit.js
+++ EnterExit.js
@@ -1,26 +1,46 @@
 let user_in = 0; //입장전 상태
+let user_num = 0;
 function saveUser() {
+  localStorage.setItem("user_num", JSON.stringify(user_num));
   localStorage.setItem("user_in", JSON.stringify(user_in));
-  //console.log("save " + user_in);
+  //console.log("save " + user_num);
 }
 function loadLoginStatus() {
   let login = localStorage.getItem("user_in");
+  let num = localStorage.getItem("user_num");
   user_in = JSON.parse(login);
+  user_num = JSON.parse(num);
+  if (user_num === null) user_num = 0;
+  //console.log("load " + user_num);
+
   //console.log("load " + user_in);
 }
 
 function ChangeMaintoExit() {
+  loadLoginStatus();
+  user_num += 1;
+
+  document.getElementById("usernumber").innerText = user_num + "명";
+
   document.getElementById("beforeEnter").style.display = "none";
   document.getElementById("afterEnter").style.display = "block";
   document.getElementById("personalroutineB").style.visibility = "visible";
+
   user_in = 1; //입장하지 않은상태
   saveUser();
+
   //console.log(user_in);
 }
 function ChangeMaintoEnter() {
+  loadLoginStatus();
+  user_num -= 1;
+
+  document.getElementById("usernumber").innerText = user_num + "명";
+
   document.getElementById("beforeEnter").style.display = "block";
   document.getElementById("afterEnter").style.display = "none";
   document.getElementById("personalroutineB").style.visibility = "hidden";
+
   user_in = 0; //입장한 상태
   saveUser();
   //console.log(user_in);
@@ -34,6 +54,8 @@ function checkEntering() {
 }
 window.addEventListener("load", () => {
   loadLoginStatus();
+
+  document.getElementById("usernumber").innerText = user_num + "명";
   if (user_in === 1) {
     document.getElementById("beforeEnter").style.display = "none";
     document.getElementById("afterEnter").style.display = "block";
--- database.js
+++ database.js
@@ -1,4 +1,26 @@
-const mysql = require("mysql"); // mysql 모듈 로드
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "127.0.0.1",
+  port: "3306",
+  user: "root",
+  password: "1111",
+});
+con.connect(); // DB 접속
+var testQuery = "UPDATE `UserNum` SET `Number` = `Number`+1";
+con.query(testQuery, function (err, results, fields) {
+  // testQuery 실행
+  if (err) {
+    console.log(err);
+  }
+  console.log(results);
+});
+conn.end(); // DB 접속 종료
+/*
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 const conn = {
   // mysql 접속 설정
   host: "localhost",
@@ -32,3 +54,4 @@ connection.query(testQuery, function (err, results, fields) {
 });
 
 connection.end(); // DB 접속 종료
+*/
