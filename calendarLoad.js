a94ea3f4566680b196a38a7b159a918bbee06754
yuki <yusun91317@gmail.com>
2021-11-22 14:32:24 +0900

    localstorage done

---
+++ calendarLoad.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", () => {
+    document.querySelectorAll(".pixel").forEach((pixel) => {
+      /*달력에서 클릭된 날짜의 id(ex. 2021-11-11)을 가져와서 나머지 파트의 현재 날짜로 설정*/
+      pixel.addEventListener("click", () => {
+        let select_date = pixel.id;
+        document.getElementById("selected_date").innerHTML = select_date;
+        document.getElementById("selected_date_workout").innerHTML = select_date;
+        document.getElementById("selected_date_diet").innerHTML = select_date;
+        document.getElementById("add_diet_date").innerHTML = select_date;
+      });
+    });
+  });
