65c9b35871e04f7e9b9bbaac04305452663f5f26
Steve <klop9090@g.skku.edu>
2021-11-24 01:55:15 +0900

    More detailed ID specification based on the selected date

--- add_diet.js
+++ add_diet.js
@@ -3,6 +3,11 @@ function storeDiet() {
     var input = document.getElementById("menu").value;
     var input2 = document.getElementById("weight").value;
     var dummy = document.querySelectorAll('input[name="meal"]');
+    var selectedDate = document.getElementById("selected_date_diet").innerText;
+    if (selectedDate === '') {
+      alert("Select Date!");
+      return;
+    }
     let selectedTime;
     for (let b of dummy) {
         if(b.checked){
@@ -10,19 +15,19 @@ function storeDiet() {
             break;
         }
     }
-    
-    if (localStorage.getItem("DietList") === null) {
+    var targetID = selectedDate + "_DietList"
+
+    if (localStorage.getItem(targetID) === null) {
       var dietGraph = ('{"아침":' + '[]' + ',"점심":' + '[]' + ',"저녁":' + '[]' + '}')
-      localStorage.setItem("DietList", dietGraph)
+      localStorage.setItem(targetID, dietGraph)
     }
 
-    var old = JSON.parse(localStorage.getItem("DietList"))
+    var old = JSON.parse(localStorage.getItem(targetID))
     var text = JSON.parse(('{"메뉴":"' + input + '","무게":' + input2 + '}'));
 
     old[selectedTime].push(text);
     var strold = JSON.stringify(old)
-    localStorage.setItem("DietList", strold)
-    // window.close("diet.html");
+    localStorage.setItem(targetID, strold)
 }
 
 function diet() {
@@ -62,43 +67,52 @@ function returndiet() {
   }
 }
 
-function getDietInfo() {
-  var storage = JSON.parse(localStorage.getItem("DietList"));
-  var breakfastList = storage["아침"];
-  var lunchList = storage["점심"];
-  var dinnerList = storage["저녁"];
+function getDietInfo(randomparameter) {
+  if(typeof randomparameter === "undefined") {
+    randomparameter = document.getElementById("selected_date_diet").innerHTML;
+  }
 
+  var targetID = randomparameter + "_DietList"
+  var storage = JSON.parse(localStorage.getItem(targetID));
   var breakfastText = '';
   var lunchText = '';
   var dinnerText = '';
 
-  breakfastList.forEach(element =>{ 
-    breakfastText += element["메뉴"];
-    breakfastText += '/';
-    breakfastText += element["무게"];
-    breakfastText += '(kcal)'
-    breakfastText += '\n';
-  });
+  if (JSON.stringify(storage) !== 'null') {
+    var breakfastList = storage["아침"];
+    var lunchList = storage["점심"];
+    var dinnerList = storage["저녁"];
+  
+    breakfastList.forEach(element =>{ 
+      breakfastText += element["메뉴"];
+      breakfastText += '/';
+      breakfastText += element["무게"];
+      breakfastText += '(kcal)'
+      breakfastText += '\n';
+    });
+  
+    lunchList.forEach(element =>{ 
+      lunchText += element["메뉴"];
+      lunchText += '/';
+      lunchText += element["무게"];
+      lunchText += '(kcal)'
+      lunchText += '\n';
+    });
+  
+    dinnerList.forEach(element =>{ 
+      dinnerText += element["메뉴"];
+      dinnerText += '/';
+      dinnerText += element["무게"];
+      dinnerText += '(kcal)'
+      dinnerText += '\n';
+    });
+
+  }
+
   document.getElementById("breakfastID").innerHTML = breakfastText;
-
-  lunchList.forEach(element =>{ 
-    lunchText += element["메뉴"];
-    lunchText += '/';
-    lunchText += element["무게"];
-    lunchText += '(kcal)'
-    lunchText += '\n';
-  });
   document.getElementById("lunchID").innerHTML = lunchText;
-
-  dinnerList.forEach(element =>{ 
-    dinnerText += element["메뉴"];
-    dinnerText += '/';
-    dinnerText += element["무게"];
-    dinnerText += '(kcal)'
-    dinnerText += '\n';
-  });
   document.getElementById("dinnerID").innerHTML = dinnerText;
-
+  
 }
 
 function removeAll() {
